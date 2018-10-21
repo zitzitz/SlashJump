72b20ed3580fccfda40d88b3b0c83575caa40b56
Zitzitz <lut.sv90@gmail.com>
2018-10-22 00:30:38 +0700

    update attack and collision

---
+++ scripts/global.lua
@@ -0,0 +1,12 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+local M = {}
+
+M.window_width = nil
+M.window_height = nil
+M.height = nil
+M.Width = nil
+
+return M
